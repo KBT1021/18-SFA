e3e921e91dac923f126adfe6f05224d00762118a
KBT1021 <yuuki@kubotayuukinoMacBook-Pro.local>
2018-07-15 02:31:54 +0900

    座標軸の設定，エディタ入力部分
    座標軸をひとまずCubeで作成した。エディタについてはtextに出力するところまで。これをfloatに変換して，数式として解釈し表示するところが
    次。

--- 3DGraphView/Assets/Scripts/InputManager.cs
+++ 3DGraphView/Assets/Scripts/InputManager.cs
@@ -6,18 +6,24 @@ using UnityEngine.UI;
 public class InputManager : MonoBehaviour {
     InputField inputField;
 
+    public Text text;
+
 	// Use this for initialization
 	void Start () {
         inputField = GetComponent<InputField>();
         InitInputField();
 	}
 
-    public void InputLogger(){
-        string inputValue = inputField.text;
-        Debug.Log(inputValue);
+    public void InputSaver(){
+        string inputString = inputField.text;
+        inputString = inputField.text;
+        text.text = inputString;
+        Debug.Log(inputString);
         InitInputField();
     }
+
 	
+
     void InitInputField(){
         inputField.text = "";
         inputField.ActivateInputField();
